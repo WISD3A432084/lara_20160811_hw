f872f390053333a8ddd4ddc6845657732e21a165
Xin <lovemarks19@gmail.com>
2017-10-15 16:53:47 +0800

    設定 '/' 的route name，並對應到 HomeController

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',['as' => 'home.index', 'uses' => 'HomeController@index']);
 
 Route::get('/about', function () {
     return view('welcome');
